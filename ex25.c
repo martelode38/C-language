5e3a0c33ee0ed174aff9e647d6191eab69294097
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:11:19 -0300

    Create ex25.c

---
+++ ex25.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//MARCOS MARTENIER SANTOS OLIVEIRA
+void main()
+{
+    float n1 = 0, n2 = 0, nota_final;
+
+    scanf("%f%f", &n1,&n2);
+
+
+        nota_final = ((n1*2)+(n2*3))/5;
+
+        printf("%.2f", nota_final);
+
+
+
+
+
+}
