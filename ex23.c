592e08ab5c6a4ebb4436c187f2bd68968696a2b8
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:10:22 -0300

    Create ex23.c

---
+++ ex23.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+//MARCOS MARTENIER SANTOS OLIVEIRA
+void main()
+{
+
+    int a,b,c;
+    int medGeo;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    medGeo = (a*b*c)/3;
+
+    printf("sua média foi %d", medGeo);
+
+
+}
