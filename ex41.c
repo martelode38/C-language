67932d133c63523c058c4ad29e937fd4a3bbceea
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:16:17 -0300

    Create ex41.c

---
+++ ex41.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+float aritimetica(float nota1, float nota2, float nota3)
+{
+
+    float media;
+    media = (nota1 + nota2 + nota3)/3;
+
+    return media;
+}
+
+float ponderada(float nota1, float nota2, float nota3)
+{
+    float media;
+    media = ((nota1*5) + (nota2*3) + (nota3*2))/9;
+
+    return media;
+}
+
+float harmonica(float nota1, float nota2, float nota3){
+
+    float mediaHarmonica =0;
+    mediaHarmonica = 3/((1/nota1)+(1/nota2)+(1/nota3));
+
+    return mediaHarmonica;
+
+}
+
+
+int main()
+{
+    char letra;
+    int x, y, z;
+
+    scanf("%d%d%d %c",&x,&y,&z,&letra);
+
+    if(letra == 'a'){
+
+        printf("%f",aritimetica(x,y,z));
+    }else if(letra == 'p'){
+        printf("%f",ponderada(x,y,z));
+    }else if(letra == 'h'){
+        printf("%f",harmonica(x,y,z));
+    }
+}
