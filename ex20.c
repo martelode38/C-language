e3ad97261c40d24fb30cd61bc6b225c4e983aaf4
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:09:15 -0300

    Create ex20.c

---
+++ ex20.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+//L1 Q1
+//MARCOS MARTENIER SANTOS OLIVEIRA
+void main(){
+
+    int a,b,c,d;
+
+    int q,w,e,r;
+
+
+    scanf("%d %d %d %d", &a,&b,&c,&d);
+
+    printf("%d %d %d %d", a,b,c,d);
+
+    a = a+1;
+    b = b+1;
+    c = c+1;
+    d = d+1;
+
+    printf("\n%d %d %d %d", a,b,c,d);
+
+}
