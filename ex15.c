48d6cd4a742f7fd57fad96cc4f9ffe8536e040a1
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:06:10 -0300

    Create ex15.c

---
+++ ex15.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main(){
+
+    int a,b;
+
+    scanf("%d%d", &a ,&b);
+
+    for(int i = 0; i < a; i++){
+
+
+        for( int j = 0; j < b; j++){
+
+            printf("*");
+
+        }
+        printf("\n");
+
+    }
+
+}
