cd31925a85747d70d45d48cc537e5e8e309fb355
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:09:58 -0300

    Create ex21.c

---
+++ ex21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//L1 Q2
+//MARCOS MARTENIER SANTOS OLIVEIRA
+void main()
+{
+
+    int a,b,c;
+    int media;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    media = (a + b + c)/3;
+
+    printf("sua média foi %d", media);
+
+
+
+
+}
