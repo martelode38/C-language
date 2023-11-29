f6c8eeb4268e9fdeccc73978af81aeeaa8a71f54
martelode38 <115518751+martelode38@users.noreply.github.com>
2023-11-29 00:11:07 -0300

    Create ex24.c

---
+++ ex24.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//L1 Q4
+//MARCOS MARTENIER SANTOS OLIVEIRA
+void main()
+{
+
+    int a,b,c;
+    int medPond;
+
+    scanf("%d %d %d",&a,&b,&c);
+
+    medPond = (a+(b*2)+(c*3))/6;
+
+    printf("sua média foi %d", medPond);
+
+
+
+}
